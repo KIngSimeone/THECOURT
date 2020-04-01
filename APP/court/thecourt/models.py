feab8081a9b84d7b5b7823faff67e7c50485dd3c
KingSimeone <simeoneumoh@outlook.com>
2020-04-01 22:03:31 +0100

    added models

--- APP/court/thecourt/models.py
+++ APP/court/thecourt/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from phone_field import PhoneField
 
 # Create your models here.
+
+class user(models.Model):
+    name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    phone = PhoneField(blank=True, help_text='Contct phone number')
+
+class chat(models.Model):
+    chats = models.CharField(max_length=2000)
+    pub_time = models.DateTimeField('time published')
